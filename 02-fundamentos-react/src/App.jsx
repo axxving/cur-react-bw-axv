e578fe2938e6a61d69f54e7a24cbbf3ef5c1b397
Irving Alejandro Hernández Ramírez <ihernandezramirez800@gmail.com>
2024-05-19 17:10:00 -0600

    feat(): my first component

--- 02-fundamentos-react/src/App.jsx
+++ 02-fundamentos-react/src/App.jsx
@@ -1,10 +1,15 @@
 
 export const App = () => {
+
+    const title = "Mi titulo desde una constante";
+    const classTitle = "text-center";
+    const pathImg = "https://images.pexels.com/photos/10295057/pexels-photo-10295057.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
+
     return (
         <>
-            <h2 className="title">App</h2>
+            <h2 className={classTitle}>{title.toUpperCase()}</h2>
             <p className="text-center">App component</p>
+            <img className="img" src={pathImg} alt={`imagen - ${title}`} />
         </>
-        
     )
 }
