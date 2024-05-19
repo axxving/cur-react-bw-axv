09d6bce1b78be37f3fd6a7afb64d625051ed555a
Irving Alejandro Hernández Ramírez <ihernandezramirez800@gmail.com>
2024-05-19 17:22:21 -0600

    feat(): ternary operator

--- 02-fundamentos-react/src/App.jsx
+++ 02-fundamentos-react/src/App.jsx
@@ -5,18 +5,36 @@ export const MyButton = () => {
     )
 }
 
+export const OnlineText = () => {
+    return (
+        <h1>OnlineText</h1>
+    )
+}
+
+export const OfflineText = () => {
+    return (
+        <h1>OfflineText</h1>
+    )
+}
+
+
 export const App = () => {
 
     const title = "Mi titulo desde una constante";
     const classTitle = "text-center";
     const pathImg = "https://images.pexels.com/photos/10295057/pexels-photo-10295057.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
+    const user = true;
 
     return (
-        <>
+        <div className="container">
             <h2 className={classTitle}>{title.toUpperCase()}</h2>
             <p className="text-center">App component</p>
             <img className="img" src={pathImg} alt={`imagen - ${title}`} />
             <MyButton />
-        </>
+            <br />
+            {
+                user ? <OnlineText /> : <OfflineText />
+            }
+        </div>
     )
 }
