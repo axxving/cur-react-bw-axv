64c15dee33db49dcb9d0cba363408b11a83d5acc
Irving Alejandro Hernández Ramírez <ihernandezramirez800@gmail.com>
2024-05-20 20:45:36 -0600

    feat(): onchange

--- 03-formularios-react/src/components/Controlado.jsx
+++ 03-formularios-react/src/components/Controlado.jsx
@@ -1,9 +1,16 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 
 export const Controlado = () => {
 
+    const [title, setTitle] = useState('Tarea #1');
+
+    const [description, setDescription] = useState('Descripcion de la tarea, ');
+
+    const [state, setState] = useState('pendiente');
+
     const handleSubmit = (e) => {
         e.preventDefault();
+        console.log(title, description, state)
     }
 
     return (
@@ -13,13 +20,22 @@ export const Controlado = () => {
                 placeholder='Ingrese TODO'
                 className='form-control mb-2'
                 name='title'
+                value={title}
+                onChange={e => setTitle(e.target.value)}
             />
             <textarea
                 className='form-control mb-2'
                 placeholder='Ingrese descripción'
                 name='description'
+                value={description}
+                onChange={e => setDescription(e.target.value)}
             />
-            <select className='form-select mb-2' name='state'>
+            <select 
+                className='form-select mb-2' 
+                name='state'
+                value={state}
+                onChange={e => setState(e.target.value)}
+            >
                 <option value="pendiente">Pendiente</option>
                 <option value="completado">Completado</option>
             </select>
