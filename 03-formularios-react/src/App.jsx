ff1d263f3ad77b4285153af5995bcad5cf1db929
Irving Alejandro Hernández Ramírez <ihernandezramirez800@gmail.com>
2024-05-20 21:01:40 -0600

    feat(): spread operator

--- 03-formularios-react/src/App.jsx
+++ 03-formularios-react/src/App.jsx
@@ -1,10 +1,13 @@
 import React, { useRef, useState } from 'react';
 //import { NoControlado } from './components/NoControlado';
 import { Controlado } from './components/Controlado';
+import { Cat } from './components/Cat';
 
 export const App = () => {
   return (
     // <NoControlado />
-    <Controlado />
+    <>
+      <Controlado />
+    </>
   );
 }
--- 03-formularios-react/src/components/Controlado.jsx
+++ 03-formularios-react/src/components/Controlado.jsx
@@ -2,15 +2,19 @@ import React, { useRef, useState } from 'react';
 
 export const Controlado = () => {
 
-    const [title, setTitle] = useState('Tarea #1');
+    const [todo, setTodo] = useState({
+        title: 'TODO #',
+        description: 'Descripcion',
+        state: 'Pendiente',
+    });
 
-    const [description, setDescription] = useState('Descripcion de la tarea, ');
-
-    const [state, setState] = useState('pendiente');
+    // const [title, setTitle] = useState('Tarea #1');
+    // const [description, setDescription] = useState('Descripcion de la tarea, ');
+    // const [state, setState] = useState('pendiente');
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(title, description, state)
+        console.log(todo.title, todo.description, todo.state)
     }
 
     return (
@@ -20,21 +24,21 @@ export const Controlado = () => {
                 placeholder='Ingrese TODO'
                 className='form-control mb-2'
                 name='title'
-                value={title}
-                onChange={e => setTitle(e.target.value)}
+                value={todo.title}
+                onChange={e => setTodo({...todo, title: e.target.value})}
             />
             <textarea
                 className='form-control mb-2'
                 placeholder='Ingrese descripción'
                 name='description'
-                value={description}
-                onChange={e => setDescription(e.target.value)}
+                value={todo.description}
+                onChange={e => setTodo({...todo, description: e.target.value})}
             />
             <select 
                 className='form-select mb-2' 
                 name='state'
-                value={state}
-                onChange={e => setState(e.target.value)}
+                value={todo.state}
+                onChange={e => setTodo({...todo, state: e.target.value})}
             >
                 <option value="pendiente">Pendiente</option>
                 <option value="completado">Completado</option>
