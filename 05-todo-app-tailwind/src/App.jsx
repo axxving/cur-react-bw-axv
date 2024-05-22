88ad0d4cc8fa57219705feba89086c783177f765
Irving Alejandro Hernández Ramírez <ihernandezramirez800@gmail.com>
2024-05-22 17:19:20 -0600

    feat(): add prettier tailwind
    Co-Authored-By: Irving Alejandro Hernández Ramírez <160192346+axxving@users.noreply.github.com>

--- 05-todo-app-tailwind/src/App.jsx
+++ 05-todo-app-tailwind/src/App.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export const App = () => {
   return (
-    <h1 className='text-center'>App</h1>
+    <h1 className='text-center md:text-right bg-purple-600 text-white'>App</h1>
   )
 }
