1082c4214c2ed3451384ef06d9ff06c3c602ff91
Irving Alejandro Hernández Ramírez <ihernandezramirez800@gmail.com>
2024-05-23 21:12:52 -0600

    feat(): filter

--- 05-todo-app-tailwind/src/App.jsx
+++ 05-todo-app-tailwind/src/App.jsx
@@ -36,35 +36,65 @@ export const App = () => {
       id: Date.now(),
       title: title.trim(),
       completed: false,
-    }
+    };
 
     setListaTodos([...listaTodos, newTodo]);
-  }
+  };
 
   const removeTodo = (id) => {
-    setListaTodos(listaTodos.filter((todo) => todo.id !== id))
-  }
+    setListaTodos(listaTodos.filter((todo) => todo.id !== id));
+  };
 
   const updateTodo = (id) => {
-    setListaTodos(listaTodos.map(todo => todo.id === id ? {...todo, completed: !todo.completed} : todo));
-  }
+    setListaTodos(
+      listaTodos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
 
   const computedItemsLeft = listaTodos.filter((todo) => !todo.completed).length;
 
   const clearCompleted = () => {
-    setListaTodos(listaTodos.filter((todo) => !todo.completed))
+    setListaTodos(listaTodos.filter((todo) => !todo.completed));
+  };
+
+  const [filter, setFilter] = useState('all');
+
+  const changeFilter = (filter) => setFilter(filter);
+
+  const filteredTodos = () => {
+    switch (filter) {
+      case 'all':
+        return listaTodos;
+      case 'active':
+        return listaTodos.filter((todo) => !todo.completed);
+      case 'completed':
+        return listaTodos.filter((todo) => todo.completed);
+      default:
+        return listaTodos;
+    }
   }
 
   return (
     <div className="min-h-screen bg-gray-300 bg-[url('./assets/imgs/bg-mobile-light.jpg')] bg-contain bg-no-repeat">
       <Header />
       <main className="container mx-auto mt-8 px-4">
-        <TodoCreate createTodo={createTodo}/>
-        <TodoList listaTodos={listaTodos} removeTodo={removeTodo} updateTodo={updateTodo} />
-        <TodoComputed computedItemsLeft={computedItemsLeft} clearCompleted={clearCompleted} />
-        <TodoFilter />
+        <TodoCreate createTodo={createTodo} />
+        <TodoList
+          listaTodos={filteredTodos()}
+          removeTodo={removeTodo}
+          updateTodo={updateTodo}
+        />
+        <TodoComputed
+          computedItemsLeft={computedItemsLeft}
+          clearCompleted={clearCompleted}
+        />
+        <TodoFilter changeFilter={changeFilter} filter={filter}/>
       </main>
-      <footer className="mt-8 text-center">Drag and drop to reorder list</footer>
+      <footer className="mt-8 text-center">
+        Drag and drop to reorder list
+      </footer>
     </div>
   );
 };
--- 05-todo-app-tailwind/src/components/TodoFilter.jsx
+++ 05-todo-app-tailwind/src/components/TodoFilter.jsx
@@ -1,13 +1,27 @@
-
-export const TodoFilter = () => {
+export const TodoFilter = ({ changeFilter, filter }) => {
     return (
         <div className="container mx-auto mt-8">
             <div className="flex justify-center gap-4 rounded-md bg-white p-4">
-                <button className="text-blue-600">All</button>
-                <button className="hover:text-blue-600">Active</button>
-                <button className="hover:text-blue-600">Completed</button>
+                <button
+                    className={`${filter === "all" ? "text-blue-500" : "text-gray-400"} hover:text-blue-500 transition duration-300 ease-in-out`}
+                    onClick={() => changeFilter("all")}
+                >
+                    All
+                </button>
+                <button
+                    className={`${filter === "active" ? "text-blue-500" : "text-gray-400"}
+            hover:text-blue-500 transition duration-300 ease-in-out`}
+                    onClick={() => changeFilter("active")}
+                >
+                    Active
+                </button>
+                <button
+                    className={`${filter === "completed" ? "text-blue-500" : "text-gray-400"} hover:text-blue-500 transition duration-300 ease-in-out`}
+                    onClick={() => changeFilter("completed")}
+                >
+                    Completed
+                </button>
             </div>
-
         </div>
-    )
-}
+    );
+};
