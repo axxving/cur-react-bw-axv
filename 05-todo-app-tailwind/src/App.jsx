6ddeb977b53b6a60b16fed202ac18fc099d47b77
Irving Alejandro Hernández Ramírez <ihernandezramirez800@gmail.com>
2024-05-23 19:43:09 -0600

    feat(): create todo

--- 05-todo-app-tailwind/src/App.jsx
+++ 05-todo-app-tailwind/src/App.jsx
@@ -31,14 +31,24 @@ const initialStateTodos = [
 export const App = () => {
   const [listaTodos, setListaTodos] = useState(initialStateTodos);
 
+  const createTodo = (title) => {
+    const newTodo = {
+      id: Date.now(),
+      title: title.trim(),
+      completed: false,
+    }
+
+    setListaTodos([...listaTodos, newTodo]);
+  }
+
   return (
     <div className="min-h-screen bg-gray-300 bg-[url('./assets/imgs/bg-mobile-light.jpg')] bg-contain bg-no-repeat">
       <Header />
       <main className="container mx-auto mt-8 px-4">
-        <TodoCreate setListaTodos={setListaTodos} />
+        <TodoCreate createTodo={createTodo}/>
         <TodoList listaTodos={listaTodos} />
-        <TodoComputed listaTodos={listaTodos} />
-        <TodoFilter setListaTodos={setListaTodos} />
+        <TodoComputed />
+        <TodoFilter />
       </main>
       <footer className="mt-8 text-center">Drag and drop to reorder list</footer>
     </div>
